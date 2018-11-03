0056da1fbbf4cf11fb730c87138353b0121d632c
SMCarpenter <smc1155@gmail.com>
2018-11-02 21:25:11 -0400

    Upload working completed version

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var htmlRoutes = require('./app/routing/htmlRoutes');
+var apiRoutes = require('./app/routing/apiRoutes');
+
+
+var server = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+//---------------------------------------------------------
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(express.static('app/public'));
+
+
+// Outsource the URL routing
+//---------------------------------------
+server.use('/', htmlRoutes);
+server.use('/api', apiRoutes);
+
+
+// Make the server listen
+//---------------------------------------
+server.listen(PORT, function() {
+  console.log("Server is listening on PORT " + PORT);
+});
